ec8516c6f9a596975ea1b4a73fdbba7cdc10dc20
Mdang2186 <jejangwangminh@gmail.com>
2026-02-28 22:20:55 +0700

    fix: port and cors for api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
-  // Gateway runs on 3000
-  app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`API Gateway is running on: ${await app.getUrl()}`);
 }
 bootstrap();
